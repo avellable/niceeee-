7557f549d72d1f5eb9fc9ff2d5034c623bc865be
Mahesh Baheti <mahsh.baheti@gmail.com>
2018-02-22 10:45:53 +0900

    Niceeee

---
+++ README.md
@@ -0,0 +1,1 @@
+![Nice](http://i0.kym-cdn.com/photos/images/original/000/450/154/820.jpeg)
---
+++ content_script.js
@@ -0,0 +1,42 @@
+walk(document.body);
+
+function walk(node)
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+	// if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+	//     || Array.from(node.classList).indexOf('ace_editor') > -1) {
+	// 	return;
+	// }
+
+	switch ( node.nodeType )
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode)
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\b69\b/g, "Nice!");
+
+	textNode.nodeValue = v;
+}
